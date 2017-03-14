2ec7eeaa173cfa9f843061237eada7f8d8c68a65
ankhthief <radim.bures@gmail.com>
2017-03-14 23:20:46 +0100

    příprava dialogfragment

--- app/src/main/java/radimbures/firstaidlog/EventsList.java
+++ app/src/main/java/radimbures/firstaidlog/EventsList.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,12 +29,18 @@ public class EventsList extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_events_list, container, false);
 
+        final FragmentManager fm = getFragmentManager();
+        final AddEvent addEvent = new AddEvent();
         FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager fm = getFragmentManager();
-                fm.beginTransaction().replace(R.id.fragment_holder, new ParticipantsList()).addToBackStack(null).commit();
+
+                //ukáže dialog
+                //TODO vyčistit kod
+                addEvent.show(fm, "AddEvent");
+                ///fm.beginTransaction().replace(R.id.fragment_holder, new ParticipantsList()).addToBackStack(null).commit();
+
             }
         });
 
