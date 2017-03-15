700652c71179da96395ee56cc19d860c96b1c5e0
ankhthief <radim.bures@gmail.com>
2017-03-15 21:06:59 +0100

    změna na alertfragment

--- app/src/main/java/radimbures/firstaidlog/DatabaseHelper.java
+++ app/src/main/java/radimbures/firstaidlog/DatabaseHelper.java
@@ -3,26 +3,36 @@ package radimbures.firstaidlog;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-/**
- * Created by radim on 05.03.2017.
- */
+import android.content.ContentValues;
+
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    //TODO doplnit všechny tabulky
+
+    //database info
     public static final String DATABASE_NAME = "firstaidlog.db";
-    public static final String TABLE_EVENTS = "events";
+    public static final String TABLE_EVENTS = "events";  //database name Events
+    public static final int DATABASE_VERSION = 1;  //database version. Need to increment every time DB changes
+
+
+    //table Events
     public static final String EVENTS_ROWID = "id";
+    public static final String EVENTS_NAME = "name";
+
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
         SQLiteDatabase db = this.getReadableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
+
+        //TODO možná přepsat tento příkaz
         //vytovoření tabulky Events
-        db.execSQL("create table " + TABLE_EVENTS +" ("+EVENTS_ROWID+" integer primary key autoincrement)");
+        db.execSQL("create table " + TABLE_EVENTS +" ("+EVENTS_ROWID+" integer primary key autoincrement" + EVENTS_NAME+" text not null)" );
     }
 
     @Override
@@ -32,4 +42,5 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
 
     }
+
 }
--- app/src/main/java/radimbures/firstaidlog/EventsList.java
+++ app/src/main/java/radimbures/firstaidlog/EventsList.java
@@ -1,6 +1,7 @@
 package radimbures.firstaidlog;
 
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -9,37 +10,62 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.AlertDialog;
+import android.widget.Toast;
+import android.widget.EditText;
+
+
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EventsList extends Fragment {
-
-
+public class EventsList extends DialogFragment {
 
     public EventsList() {
         // Required empty public constructor
     }
 
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_events_list, container, false);
-
         final FragmentManager fm = getFragmentManager();
-        final AddEvent addEvent = new AddEvent();
         FloatingActionButton fab = (FloatingActionButton) root.findViewById(R.id.fab);
+        final EditText eventName = (EditText) root.findViewById(R.id.add_event_name);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 //ukáže dialog
                 //TODO vyčistit kod
-                addEvent.show(fm, "AddEvent");
-                ///fm.beginTransaction().replace(R.id.fragment_holder, new ParticipantsList()).addToBackStack(null).commit();
+                //LayoutInflater layoutInflaterAndroid = LayoutInflater.from(root);
+                //View mView = layoutInflaterAndroid.inflate(R.layout.user_input_dialog_box, null);
+
+                ///fm.beginTransaction().replace(R.id.fragment_holder, new ParticipantsList()).addToBackStack(null).commit()
+                final AlertDialog.Builder addEventDialog = new AlertDialog.Builder(getContext());
+                addEventDialog.setTitle(R.string.addEventDialog);
+                View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.dialog_add_event, (ViewGroup) getView(), false);
+                addEventDialog.setView(viewInflated);
+                addEventDialog.setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //TODO zde se načtou data z polí a uloží do databáze
+                        //String str = eventName.getText().toString();
+                        Toast.makeText(getActivity(),"pokus",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+                addEventDialog.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                addEventDialog.show();
 
             }
         });
