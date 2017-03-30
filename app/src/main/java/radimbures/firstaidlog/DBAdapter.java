615b18fa97440188de3c9e52c67c2ba4f9d67c00
Radim Bureš <radim.bures@gmail.com>
2017-03-30 12:59:46 +0200

    correct listview in participants

--- app/src/main/java/radimbures/firstaidlog/DBAdapter.java
+++ app/src/main/java/radimbures/firstaidlog/DBAdapter.java
@@ -100,8 +100,8 @@ public class DBAdapter {
 
     //returns all data from table Participants
     public Cursor getAllRowsParticipant(long radek) {
-        Cursor c = db.query(true, TABLE_PARTICIPANTS, ALL_KEYS_PARTICIPANT, null, null, null, null, null, null);
-        //Cursor c = 	db.rawQuery("SELECT * FROM participants WHERE id_event = " + radek + ";", null);
+        String S = String.valueOf(radek);
+        Cursor c = db.query(true, TABLE_PARTICIPANTS, ALL_KEYS_PARTICIPANT,"idevent=?",new String[] {S}, null, null, null, null);
         if (c != null) {
             c.moveToFirst();
         }
