40395af70dbd46fe86e74c32192ae17ce9c51e49
radim <radim.bures.@gmail.com>
2017-11-11 16:11:39 +0100

    code cleanup MainActivity.java

--- app/src/main/java/radimbures/firstaidlog/MainActivity.java
+++ app/src/main/java/radimbures/firstaidlog/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         myDB =  new DBAdapter(this);
