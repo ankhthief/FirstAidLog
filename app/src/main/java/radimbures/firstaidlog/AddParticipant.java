35f1ebfe85e5a04c36778fa55dc9b0f3cf699f02
ankhthief <radim.bures@gmail.com>
2017-11-12 22:19:12 +0100

    příprava na přidání usera přes fragment místo dialogu

---
+++ app/src/main/java/radimbures/firstaidlog/AddParticipant.java
@@ -0,0 +1,50 @@
+package radimbures.firstaidlog;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AddParticipant extends Fragment {
+
+
+    public AddParticipant() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View root = inflater.inflate(R.layout.fragment_add_participant, container, false);
+
+        return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_add_participant, menu);
+        menu.findItem(R.id.action_tips).setVisible(false);
+        menu.findItem(R.id.action_about).setVisible(false);
+    }
+}
--- app/src/main/java/radimbures/firstaidlog/ParticipantDatabase.java
+++ app/src/main/java/radimbures/firstaidlog/ParticipantDatabase.java
@@ -4,10 +4,12 @@ package radimbures.firstaidlog;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,9 +23,9 @@ public class ParticipantDatabase extends Fragment {
 
     ListView participantsDatabase;
     DBAdapter myDB;
-    SimpleCursorAdapter myCursorAdapter;
     TextView tv_empty;
     FloatingActionButton fab;
+    FragmentManager fm;
 
 
     public ParticipantDatabase() {
@@ -39,9 +41,29 @@ public class ParticipantDatabase extends Fragment {
         participantsDatabase = root.findViewById(R.id.list_participants_database);
         tv_empty= root.findViewById(R.id.tv_empty);
         tv_empty.setVisibility(View.GONE);
+        fm = getActivity().getSupportFragmentManager();
         fab = root.findViewById(R.id.fab_event);
-
         populateListView();
+
+        participantsDatabase.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                AddParticipant fragment = new AddParticipant();
+                Bundle bundle = new Bundle();
+                bundle.putLong("idparticipant", l);
+                fragment.setArguments(bundle);
+                fm.beginTransaction().replace(R.id.fragment_holder, fragment).addToBackStack(null).commit();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //přidání usera
+                fm.beginTransaction().replace(R.id.fragment_holder, new AddParticipant()).addToBackStack(null).commit();
+            }
+        });
 
         return root;
     }
