1a59510ed726f20d6ac528eaca31054a3bba26c8
ankhthief <radim.bures@gmail.com>
2017-11-30 10:58:39 +0100

    rate app

--- app/src/main/java/radimbures/firstaidlog/AboutApp.java
+++ app/src/main/java/radimbures/firstaidlog/AboutApp.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutApp extends Fragment {
+
+    Button rate;
 
 
     public AboutApp() {
@@ -23,7 +26,20 @@ public class AboutApp extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_app, container, false);
+        View root = inflater.inflate(R.layout.fragment_about_app, container, false);
+
+        rate = root.findViewById(R.id.rate_button);
+
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO přesměrovat na hodnocení Google Play
+            }
+        });
+
+
+
+        return root;
     }
 
 }
